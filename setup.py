1ba218243f304427e31a994521d8cec4d061d133
Stephan Meighen-Berger <stephan.meighenberger@gmail.com>
2021-11-19 13:54:12 +0100

    Removed pickle

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
         "PyYAML",
         "numpy",
         "scipy",
-        "pickle"
     ],
     extras_require={
         "interactive": ["nbstripout", "matplotlib", "jupyter", "tqdm"],
